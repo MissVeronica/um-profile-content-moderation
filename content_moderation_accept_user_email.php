2d629f17d364a80517586cd6204acefebe9da499
MissVeronica <veronica@tranza.se>
2023-06-07 12:27:20 +0200

    Add files via upload

---
+++ content_moderation_accept_user_email.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template for the "Content Moderation - User Accept Notification".
+ * Whether to send the user an email when his account is updated.
+ *
+ * This template is saved at your custom location {your-theme}/ultimate-member/email/content_moderation_accept_user_email.php
+ *
+ * @version 2.0.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+<div style="max-width: 560px;padding: 20px;background: #ffffff;border-radius: 5px;margin: 40px auto;font-family: Open Sans,Helvetica,Arial;font-size: 15px;color: #666" data-darkreader-inline-bgcolor="" data-darkreader-inline-bgimage="" data-darkreader-inline-color="">
+<div style="color: #444444;font-weight: normal" data-darkreader-inline-color="">
+<div style="text-align: center;font-weight: 600;font-size: 26px;padding: 10px 0;border-bottom: solid 3px #eeeeee" data-darkreader-inline-border-bottom="">{site_name}</div>
+<div style="clear: both"> </div>
+</div>
+<div style="padding: 0 30px 30px 30px;border-bottom: 3px solid #eeeeee" data-darkreader-inline-border-bottom="">
+<div style="padding: 30px 0;font-size: 24px;text-align: center;line-height: 40px">Your account has been manually reviewed<br />and your recent Profile update<br />has been accepted.</div>
+<div style="padding: 15px;background: #eee;border-radius: 3px;text-align: center" data-darkreader-inline-bgcolor="" data-darkreader-inline-bgimage="">You can now login again to your Profile .</div>
+</div>
+<div style="color: #999;padding: 20px 30px" data-darkreader-inline-color="">
+<div>Thank you!</div>
+<div>The <a style="color: #3ba1da;text-decoration: none" href="{site_url}" data-darkreader-inline-color="">{site_name}</a> Team</div>
+</div>
+</div>
